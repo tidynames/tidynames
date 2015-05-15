887e11387a014d1f46745895f3bba351bb212068
Vadim Landa <vadim.landa@transferwise.com>
2015-05-15 18:49:23 +0300

    Implemented full_name updates

--- app/action/add.js
+++ app/action/add.js
@@ -2,7 +2,7 @@ var SQL = require(global.rootPath + '/app/modules/sql');
 var Promise = require('promise');
 
 function addName(name, type) {
-	return SQL.query("INSERT INTO name(value, type) VALUES($1, $2);", [name, type]);
+	return SQL.query("INSERT INTO name(value, type) VALUES($1, $2) returning id", [name, type]);
 }
 
 
@@ -11,19 +11,23 @@ module.exports = function(req, res){
 	var lastName = req.body.lastName;
 
 	var promises = [];
-
+	var personId = parseInt(Math.random() * 1000000);
 	firstName.toLowerCase().split(' ').forEach(function(name) {
 		if(!name.length) {
 			return;
 		}
-		promises.push(addName(name, 'F'));
+		promises.push(addName(name, 'F').then(function(id)  {
+			return SQL.query("INSERT INTO full_name(person_id, name_id, ordering) VALUES($1, $2, 1)", [personId, id.rows[0].id])
+		}));
 	});
 
 	lastName.toLowerCase().split(' ').forEach(function(name) {
 		if(!name.length) {
 			return;
 		}
-		promises.push(addName(name, 'L'));
+		promises.push(addName(name, 'L').then(function(id)  {
+			return SQL.query("INSERT INTO full_name(person_id, name_id, ordering) VALUES($1, $2, 2)", [personId, id.rows[0].id])
+		}));
 	});
 
 	var name = lastName + ', ' + firstName;
