ca042d520c8936463542190e4ad3c2759c8c190a
Dmitri Timašjov <dmitri.timasjov@transferwise.com>
2015-05-15 13:57:19 +0300

    lookup form

--- index.js
+++ index.js
@@ -19,47 +19,16 @@ app.get('/lookup', function (req, res) {
 	if (req.query.name)  {
         var parts = req.query.name.split(' ');
         for (p in parts)  {
-            lookup(parts[p].replace(/\s/g, '').toLower());
+            //lookup(parts[p].replace(/\s/g, '').toLower());
         }
     }
 	res.send('Hello World!');
 });
 
 app.post('/add',function(req, res){
-	var name = req.body.name;
-	console.log(name);
-	res.end("some information");
-});
-
-app.get('/:name?', function (req, res) {
-  if (req.params.name)  {
-      var dummy = 'John';
-      var result = "Dice: " +
-	  clj_fuzzy.metrics.dice(req.params.name, dummy) + "<br>" +
-      "Sorensen: " +
-	  clj_fuzzy.metrics.sorensen(req.params.name, dummy) + "<br>" +
-      "Levenshtein: " +
-	  clj_fuzzy.metrics.levenshtein(req.params.name, dummy) + "<br>" +
-      "Hamming: " +
-	  clj_fuzzy.metrics.hamming(req.params.name, dummy) + "<br>" +
-      "Jaccard: " +
-	  clj_fuzzy.metrics.jaccard(req.params.name, dummy) + "<br>" +
-      "Tanimoto: " +
-	  clj_fuzzy.metrics.tanimoto(req.params.name, dummy) + "<br>" +
-      "Jaro: " +
-	  clj_fuzzy.metrics.jaro(req.params.name, dummy) + "<br>" +
-      "Jaro-Winckler: " +
-	  clj_fuzzy.metrics.jaro_winkler(req.params.name, dummy) + "<br>" +
-      "MRA: " +
-	  clj_fuzzy.metrics.mra_comparison(req.params.name, dummy) + "<br>" +
-      "Tversky: " +
-	  clj_fuzzy.metrics.tversky(req.params.name, dummy);
-      res.send(result);
-  }
-  else  {
-    res.send('Please specify a word to check!');
-  }
-
+	var firstName = req.body.firstName;
+	var lastName = req.body.lastName;
+	res.end("success");
 });
 
 var server = app.listen(3000, function () {
