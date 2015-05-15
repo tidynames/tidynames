85a7b56dc41a3d8770da4352452af07760b681a2
Dmitri Timašjov <dmitri.timasjov@transferwise.com>
2015-05-15 12:58:17 +0300

    rest template for lookup and add services

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 var express = require('express');
+var bodyParser = require("body-parser");
 var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+
+app.get('/lookup', function (req, res) {
+	console.log('name: ' + req.query.name);
+	res.send('Hello World!');
+});
+
+app.post('/add',function(req, res){
+	var name = req.body.name;
+	console.log(name);
+	res.end("some information");
 });
 
 var server = app.listen(3000, function () {
